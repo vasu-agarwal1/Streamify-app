dde7de3c0c1f510db4c6c13389d866d851bcff71
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-08-19 22:55:54 +0530

    solved some bugs

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 import cookieParser from "cookie-parser"
 
 const app = express()
@@ -12,6 +13,14 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+//routes import
+
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
 
 
 export { app }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config({
     path: '../.env'
 })
+import {app} from "./app.js"
 
 import connectDB from "./db/index.js";
 
