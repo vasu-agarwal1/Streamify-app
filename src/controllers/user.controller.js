952a752d00fd14fe22faf324e02886ee7b979bf8
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-08-22 23:00:22 +0530

    added functionalities to logout the user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -172,8 +172,34 @@ const loginUser =asyncHandler( async (req, res) => {
 
 })
 
+const logoutUser = asyncHandler(async (req,res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User loggedout successfully"))
+}) 
+
 
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }
