325c94fd045374badc7ae27da8b05831c3f652a8
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-08-25 17:34:24 +0530

    add getUserChannelProfile functionality to fetch user channel details with subscription info

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import { Subscription } from "../models/subscription.model.js";
 
 
 //we have created a method to generate tokens so that we do not have to do it again and again
@@ -355,6 +356,81 @@ const updateCoverImage = asyncHandler(async (req, res) => {
     return res
     .status(200)
     .json(new ApiResponse(200, updatedCoverImage, "cover image is updated Successfully"))
+})
+
+const getUserChannelProfile = asyncHandler(async (req, res) => 
+{
+
+     const {username} = req.params
+
+     if (!username?.trim()) {
+        throw new ApiError(400, "Username do not exist")
+     }
+
+     const channel = await User.aggregate([
+        {
+            $match: {
+                username: username.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscription",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscription",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {//adding both fields to initial db
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscibers"
+                },
+                channelsSubscribed: {
+                    $size: "$subscibedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribed: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
+     ])
+
+     if (!channel?.length) {
+        throw new ApiError(404, "channel does not exist")
+     }
+
+     console.log(channel);
+
+     return res
+     .status(200)
+     .json(new ApiResponse(200, channel[0], "User Channel fetched successfully"))
+
+    
 })
 
 
