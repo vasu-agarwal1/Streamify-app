c76a9aa259c5848ba26298d35fdc568da410f852
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-08-25 18:49:17 +0530

    add changeCurrentPassword, getCurrentUser, and updateUser routes to user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateCurrentUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -23,5 +23,9 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-Token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/get-user").post(verifyJWT,getCurrentUser)
+router.route("/update-user").post(verifyJWT,updateCurrentUser)
+
 
 export default router
