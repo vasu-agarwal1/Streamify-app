bc9501efb1de409f45a68812cc353e21af8e3593
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-08-26 19:31:23 +0530

    feat: define Playlist model with description, name, owner, and videos fields

--- src/models/playlist.model.js
+++ src/models/playlist.model.js
@@ -1,5 +1,27 @@
 import mongoose,{Schema} from "mongoose";
 
-const playlistSchema = new Schema({},{timestamps: true})
+const playlistSchema = new Schema(
+    {
+        description: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+            
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+        videos: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ]
+    },
+    {timestamps: true})
 
-export const playlist = mongoose.model("playlist", playlistSchema)
+export const Playlist = mongoose.model("Playlist", playlistSchema)
