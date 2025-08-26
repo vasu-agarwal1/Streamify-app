82a902064079c6ef352f6bfeb70e15caf5b96398
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-08-26 16:32:39 +0530

    fix: update update-user route to use PATCH method and add routes for changing avatar and cover image

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateCurrentUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateAvatar, updateCoverImage, updateCurrentUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -25,7 +25,10 @@ router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-Token").post(refreshAccessToken)
 router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 router.route("/get-user").post(verifyJWT,getCurrentUser)
-router.route("/update-user").post(verifyJWT,updateCurrentUser)
+router.route("/update-user").patch(verifyJWT,updateCurrentUser)
+
+router.route("/change-avatar").patch(verifyJWT,upload.single("avatar"), updateAvatar)
+router.route("/change-cover-image").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
 
 
 export default router
