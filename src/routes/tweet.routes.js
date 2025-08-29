957d9200fa5556b2eb5accaa896e8dcd047723e5
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-08-29 23:58:28 +0530

    feat: add updateTweet route to tweet routes

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createTweet, getUserTweets } from "../controllers/tweet.controller.js";
+import { createTweet, getUserTweets, updateTweet } from "../controllers/tweet.controller.js";
 
 
 const router = Router()
@@ -9,5 +9,6 @@ router.use(verifyJWT)
 
 router.route("/").post(createTweet)
 router.route("/user/:userId").get(getUserTweets);
+router.route("/:tweetId").get(updateTweet)
 
 export default router
