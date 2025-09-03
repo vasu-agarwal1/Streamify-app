9057994b8b2193734752a616065845ee8d74fe16
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-09-03 17:26:19 +0530

    feat: add subscription management functionality with toggle

---
+++ src/controllers/subscription.controller.js
@@ -0,0 +1,54 @@
+import mongoose, {isValidObjectId} from "mongoose"
+import {User} from "../models/user.model.js"
+import { Subscription } from "../models/subscription.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+
+const toggleSubscription = asyncHandler(async (req, res) => {
+    const {channelId} = req.params
+    // TODO: toggle subscription
+    const subscriberId = req.user._id
+
+    const subDetail = await Subscription.findOne({
+        subscriber: subscriberId,
+        channel: channelId
+    })
+
+    let subscriptionStatus;
+    if(subDetail){
+       await subDetail.deleteOne()
+       subscriptionStatus = {isSubscribed: false} 
+       return res
+        .status(200)
+        .json(new ApiResponse(200, subscriptionStatus, "Channel UnSubscribed"))
+    
+    }else{
+        await Subscription.create({
+            subscriber: subscriberId,
+            channel: channelId
+        })
+        subscriptionStatus = {isSubscribed: true}
+
+        return res
+        .status(200)
+        .json(new ApiResponse(200, subscriptionStatus, "Channel Subscribed"))
+    }
+})
+
+// controller to return subscriber list of a channel
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+    const {channelId} = req.params
+})
+
+// controller to return channel list to which user has subscribed
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+    const { subscriberId } = req.params
+})
+
+export {
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels
+}
