7227b717bccf7abbe075e391cf67076d6590ca14
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-09-05 16:28:29 +0530

    add update and delete comment functionality in comment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -35,10 +35,54 @@ const addComment = asyncHandler(async (req, res) => {
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    const {commentId} = req.params
+    const {newContent} = req.body
+
+    if(!newContent || newContent.trim() == ""){
+        throw new ApiError(400, "Please give content")
+    }
+
+    const newComment  = await Comment.findOneAndUpdate(
+        {
+        _id: commentId,
+        owner: req.user?._id
+      },
+      {
+        $set: {
+            content: newContent
+        }
+      },
+      {new: true}
+
+    )
+
+if (!newComment) {
+        throw new ApiError(404, "Error in updating new commentin DB")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, newComment, "comment updated"))
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const {commentId} = req.params
+
+    const comment = await Comment.findOneAndDelete(
+        {
+            _id : commentId,
+            owner: req.user?._id
+        }
+    )
+
+    if (!comment) {
+        throw new ApiError(400, "unauthorized access or Error while deleting in database")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {} ,"Comment Successfully Deleted"))
 })
 
 export {
