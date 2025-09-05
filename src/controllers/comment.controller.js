843132c74e10ebf9f2fa20575b7c44735bce036b
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-09-05 16:27:20 +0530

    feat: implement comment functionality with routes to add and retrieve comments for videos

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose"
+import {Comment} from "../models/comment.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+const getVideoComments = asyncHandler(async (req, res) => {
+    //TODO: get all comments for a video
+    const {videoId} = req.params
+    const {page = 1, limit = 10} = req.query
+
+})
+
+const addComment = asyncHandler(async (req, res) => {
+    // TODO: add a comment to a video
+    const {content} = req.body
+    const {videoId} = req.params
+    if(!content || content.trim() == ""){
+        throw new ApiError(400, "Please give content")
+    }
+
+    const newComment = await Comment.create({
+        owner: req.user?._id,
+        video: videoId,
+        content: content
+    })
+    if (!newComment) {
+        throw new ApiError(404, "Error in createng new commentin DB")
+    }
+
+    return res
+    .status(201)
+    .json(new ApiResponse(201, newComment, "New comment added"))
+})
+
+const updateComment = asyncHandler(async (req, res) => {
+    // TODO: update a comment
+})
+
+const deleteComment = asyncHandler(async (req, res) => {
+    // TODO: delete a comment
+})
+
+export {
+    getVideoComments, 
+    addComment, 
+    updateComment,
+    deleteComment
+    }
