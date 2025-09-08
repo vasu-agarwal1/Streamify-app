b7cb91d9fcba8775587693fdf106205c2ef3855d
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-09-08 15:22:14 +0530

    added dashboard routes for channel statistics and videos

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ import subscriptionRouter from "./routes/subscription.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import commentRouter from "./routes/comment.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from './routes/dashboard.routes.js'
 
 //routes declaration
 app.use("/api/v1/users", userRouter)// here /users is telling url to go to userRouter 
@@ -33,6 +34,7 @@ app.use("/api/v1/subscriptions", subscriptionRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/playlists", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 
 export { app }
