6a26b9bf4b90e4283a596a1405096525b3d2733c
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-12-24 17:19:56 +0530

    Refactor video routes for improved readability and consistency

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -3,30 +3,33 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { deleteVideo, getAllVideos, getVideoById, publishAVideo, togglePublishStatus, updateVideo } from "../controllers/video.controller.js";
 
-const router = Router()
+const router = Router();
 
-router.use(verifyJWT)
+router.route("/").get(getAllVideos);
+router.route("/:videoId").get(getVideoById);
+
+
+router.use(verifyJWT); 
 
 router.route("/")
-.get(getAllVideos)
-.post(
-    upload.fields([
-        {
-            name: "videoFile",
-            maxCount: 1
-        },
-        {
-            name: "thumbnail",
-            maxCount: 1
-        }
-    ]), publishAVideo
-)
+    .post(
+        upload.fields([
+            {
+                name: "videoFile",
+                maxCount: 1
+            },
+            {
+                name: "thumbnail",
+                maxCount: 1
+            }
+        ]),
+        publishAVideo
+    );
 
 router.route("/:videoId")
-.get(getVideoById)
-.delete(deleteVideo)
-.patch(upload.single("thumbnail"), updateVideo)
+    .delete(deleteVideo)
+    .patch(upload.single("thumbnail"), updateVideo);
 
 router.route("/toggle/:videoId").patch(togglePublishStatus);
 
-export default router
+export default router;
